c7e239983f6d2c1bfe7f495a0edd6e31f015d3f0
Robin Han <hanxvdovehx@gmail.com>
2025-05-06 21:01:19 +0800

    feat(table_topic): adapt to jdk11
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- core/src/main/java/kafka/automq/table/CredentialProviderHolder.java
+++ core/src/main/java/kafka/automq/table/CredentialProviderHolder.java
@@ -1,13 +1,31 @@
-package kafka.automq.table;
+/*
+ * Copyright 2025, AutoMQ HK Limited.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.automq.enterprise.kafka.auth.AliyunInstanceProfileCredentialsProvider;
-import com.automq.enterprise.kafka.auth.HuaweiCloudInstanceProfileCredentialsProvider;
-import com.automq.enterprise.kafka.auth.TencentCloudInstanceProfileCredentialsProvider;
+package kafka.automq.table;
 import com.automq.stream.s3.operator.BucketURI;
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
+
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
@@ -47,15 +65,6 @@ public class CredentialProviderHolder implements AwsCredentialsProvider {
                 switch (bucketURI.protocol()) {
                     case "s3":
                         instanceProfileCredentialsProvider = InstanceProfileCredentialsProvider.builder().build();
-                        break;
-                    case "oss":
-                        instanceProfileCredentialsProvider = AliyunInstanceProfileCredentialsProvider.builder().build();
-                        break;
-                    case "obs":
-                        instanceProfileCredentialsProvider = HuaweiCloudInstanceProfileCredentialsProvider.builder().build();
-                        break;
-                    case "cos":
-                        instanceProfileCredentialsProvider = TencentCloudInstanceProfileCredentialsProvider.builder().build();
                         break;
                     default:
                         throw new IllegalArgumentException("Unsupported protocol: " + bucketURI.protocol());
